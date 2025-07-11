dde565fa2dc808a1605486f9e376b3cc01549188
HUIJAEKO <xrhgmlwox@naver.com>
2025-07-11 17:44:45 +0900

    fix: [057-elasticsearch] 검색 엔진 임시 삭제

--- src/main/java/project/votebackend/controller/search/IndexAdminController.java
+++ src/main/java/project/votebackend/controller/search/IndexAdminController.java
@@ -12,27 +12,27 @@ import project.votebackend.service.elasticsearch.IndexAdminService;
 @RequestMapping("/admin")
 public class IndexAdminController {
 
-    private final IndexAdminService indexAdminService;
-
-    //Elasticsearch 투표 인덱싱
-    @PostMapping("/create-votes-index")
-    public ResponseEntity<String> createVotesIndex() {
-        try {
-            indexAdminService.createVotesIndex();
-            return ResponseEntity.ok("votes 인덱스 생성 성공!");
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("vote 인덱스 생성 실패: " + e.getMessage());
-        }
-    }
-
-    //Elasticsearch 유저 인덱싱
-    @PostMapping("/create-users-index")
-    public ResponseEntity<String> createUsersIndex() {
-        try {
-            indexAdminService.createUsersIndex();
-            return ResponseEntity.ok("users 인덱스 생성 성공!");
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("users 인덱스 생성 실패: " + e.getMessage());
-        }
-    }
+//    private final IndexAdminService indexAdminService;
+//
+//    //Elasticsearch 투표 인덱싱
+//    @PostMapping("/create-votes-index")
+//    public ResponseEntity<String> createVotesIndex() {
+//        try {
+//            indexAdminService.createVotesIndex();
+//            return ResponseEntity.ok("votes 인덱스 생성 성공!");
+//        } catch (Exception e) {
+//            return ResponseEntity.status(500).body("vote 인덱스 생성 실패: " + e.getMessage());
+//        }
+//    }
+//
+//    //Elasticsearch 유저 인덱싱
+//    @PostMapping("/create-users-index")
+//    public ResponseEntity<String> createUsersIndex() {
+//        try {
+//            indexAdminService.createUsersIndex();
+//            return ResponseEntity.ok("users 인덱스 생성 성공!");
+//        } catch (Exception e) {
+//            return ResponseEntity.status(500).body("users 인덱스 생성 실패: " + e.getMessage());
+//        }
+//    }
 }
