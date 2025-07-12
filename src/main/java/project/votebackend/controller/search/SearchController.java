9f9bd0021de767730df8cbe365f90c73c22ad850
HUIJAEKO <xrhgmlwox@naver.com>
2025-07-12 09:44:41 +0900

    feat: [058-search-query] 검색 쿼리문 변환

--- src/main/java/project/votebackend/controller/search/SearchController.java
+++ src/main/java/project/votebackend/controller/search/SearchController.java
@@ -1,10 +1,14 @@
 package project.votebackend.controller.search;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import project.votebackend.dto.vote.VoteSearchResponse;
 import project.votebackend.elasticSearch.UserDocument;
 import project.votebackend.elasticSearch.VoteDocument;
 import project.votebackend.service.elasticsearch.SearchService;
@@ -19,11 +23,20 @@ public class SearchController {
 
     private final SearchService searchService;
 
-    //투표 검색어 입력
+    //쿼리문을 이용한 투표 검색
     @GetMapping("/vote")
-    public List<VoteDocument> searchVotes(@RequestParam("keyword") String keyword) throws IOException {
-        return searchService.searchVotes(keyword);
+    public Page<VoteSearchResponse> searchVotes(
+            @RequestParam("keyword") String keyword,
+            @PageableDefault(size = 20, page = 0) Pageable pageable
+    ) {
+        return searchService.searchVotes(keyword, pageable);
     }
+
+//    //투표 검색어 입력
+//    @GetMapping("/vote")
+//    public List<VoteDocument> searchVotes(@RequestParam("keyword") String keyword) throws IOException {
+//        return searchService.searchVotes(keyword);
+//    }
 
     //유저 검색어 입력
     @GetMapping("/user")
