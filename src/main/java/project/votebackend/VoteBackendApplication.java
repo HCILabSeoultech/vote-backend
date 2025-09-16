300b8c4f96c5745a1f51072de8bd34225621b96f
HUIJAEKO <xrhgmlwox@naver.com>
2025-09-16 17:30:40 +0900

    feat: [039-notification] @EnableAsync 어노테이션 추가

--- src/main/java/project/votebackend/VoteBackendApplication.java
+++ src/main/java/project/votebackend/VoteBackendApplication.java
@@ -3,11 +3,13 @@ package project.votebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableScheduling
+@EnableAsync
 public class VoteBackendApplication {
 
     public static void main(String[] args) {
