549e7f7e5491d3af701f1e595e0475961d686c87
HUIJAEKO <xrhgmlwox@naver.com>
2025-10-29 19:08:54 +0900

    del: [040-edit-mypage] 기존 유저 조회 로직 삭제

--- src/main/java/project/votebackend/repository/voteStat/VoteStatRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package project.votebackend.repository.voteStat;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface VoteStatRepositoryCustom {
-    List<Object[]> getDailyVoteStats(Long userId, LocalDateTime fromDate);
-}
